ed4886e1c993bbe6a8d69e911a3747688009d8f8
fizzi01 <federico.izzi@studenti.unisalento.it>
2024-04-22 13:16:51 +0200

    Remove unused classes

--- src/main/java/it/unisalento/pasproject/taskmanagerservice/exceptions/UtenteNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package it.unisalento.pasproject.taskmanagerservice.exceptions;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND)
-public class UtenteNotFoundException extends Exception {
-}
