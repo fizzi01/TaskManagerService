fdfcd7f466b499c46d374ef247f0874a7da1fc7a
Fediz01 <federico.izzi@studenti.unisalento.it>
2024-04-22 16:14:53 +0200

    Minor changes Workflow labeling

--- src/main/java/it/unisalento/pasproject/taskmanagerservice/service/RabbitMQProducer.java
+++ src/main/java/it/unisalento/pasproject/taskmanagerservice/service/RabbitMQProducer.java
@@ -3,6 +3,7 @@ package it.unisalento.pasproject.taskmanagerservice.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -28,6 +29,7 @@ public class RabbitMQProducer {
     /**
      * RabbitTemplate instance for sending messages to RabbitMQ.
      */
+    @Autowired
     private RabbitTemplate rabbitTemplate;
 
     /**
