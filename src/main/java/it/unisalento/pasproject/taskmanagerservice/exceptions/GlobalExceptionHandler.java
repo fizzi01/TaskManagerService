afb640fb944fa358b494eb436e1e8a3149b3583d
fizzi01 <federico.izzi@studenti.unisalento.it>
2024-05-16 22:11:40 +0200

    Adapted global exception handler to handle all the exceptions with the same format

--- src/main/java/it/unisalento/pasproject/taskmanagerservice/exceptions/GlobalExceptionHandler.java
+++ src/main/java/it/unisalento/pasproject/taskmanagerservice/exceptions/GlobalExceptionHandler.java
@@ -2,10 +2,17 @@ package it.unisalento.pasproject.taskmanagerservice.exceptions;
 
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
 
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
@@ -17,5 +24,16 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(exception.getErrorResponse().getStatus()).body(exception.getErrorResponse());
     }
 
+    // Gestione generale di tutte le eccezioni predefinite
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
+        CustomErrorResponse errorResponse = CustomErrorResponse.builder()
+                .traceId(UUID.randomUUID().toString())
+                .timestamp(OffsetDateTime.now().toString())
+                .status(HttpStatus.resolve(statusCode.value()))
+                .message(ex.getMessage())
+                .build();
 
+        return ResponseEntity.status(errorResponse.getStatus()).body(errorResponse);
+    }
 }
