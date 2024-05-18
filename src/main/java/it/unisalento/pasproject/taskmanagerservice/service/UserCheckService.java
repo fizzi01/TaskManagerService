574c87c9aaf1bccfeb9b68512540025aef575486
Fediz01 <federico.izzi@studenti.unisalento.it>
2024-05-18 14:45:06 +0200

    CI/CD Update * Minor fixes * Code upgrades

--- src/main/java/it/unisalento/pasproject/taskmanagerservice/service/UserCheckService.java
+++ src/main/java/it/unisalento/pasproject/taskmanagerservice/service/UserCheckService.java
@@ -9,22 +9,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import static it.unisalento.pasproject.taskmanagerservice.security.SecurityConstants.ROLE_ADMIN;
 
 // Controlla che chi fa la richiesta sia un Utente e sia abilitato
 @Service
 public class UserCheckService {
 
-    @Value("${service.role.utente.name}")
-    public String ROLE;
 
-    @Autowired
-    private MessageExchanger messageExchanger;
-
-    @Autowired
-    @Qualifier("RabbitMQExchange")
-    private MessageExchangeStrategy messageExchangeStrategy;
+    private final MessageExchanger messageExchanger;
 
     @Value("${rabbitmq.exchange.security.name}")
     private String securityExchange;
@@ -32,12 +28,16 @@ public class UserCheckService {
     @Value("${rabbitmq.routing.security.key}")
     private String securityRequestRoutingKey;
 
+    @Autowired
+    public UserCheckService(MessageExchanger messageExchanger, @Qualifier("RabbitMQExchange") MessageExchangeStrategy messageExchangeStrategy) {
+        this.messageExchanger = messageExchanger;
+        this.messageExchanger.setStrategy(messageExchangeStrategy);
+    }
+
     private static final Logger LOGGER = LoggerFactory.getLogger(UserCheckService.class);
 
 
     public UserDetailsDTO loadUserByUsername(String email) throws UsernameNotFoundException {
-
-        messageExchanger.setStrategy(messageExchangeStrategy);
 
         //Chiamata MQTT a CQRS per ottenere i dettagli dell'utente
         UserDetailsDTO user = messageExchanger.exchangeMessage(email,securityRequestRoutingKey,securityExchange,UserDetailsDTO.class);
@@ -52,13 +52,25 @@ public class UserCheckService {
     }
 
 
-
-    public Boolean roleCheck(String role) {
-        return role.equals(ROLE);
-    }
-
     public Boolean isEnable(Boolean enable) {
         return enable;
     }
 
+    /**
+     * Check if the current user is the user with the given email
+     * @param email the email of the user to check
+     * @return true if the current user is the user with the given email, false otherwise
+     */
+    public Boolean isCorrectUser(String email){
+        return email.equals(SecurityContextHolder.getContext().getAuthentication().getName());
+    }
+
+    /**
+     * Check if the current user is an administrator
+     * @return true if the current user is an administrator, false otherwise
+     */
+    public Boolean isAdministrator(){
+        String currentRole = SecurityContextHolder.getContext().getAuthentication().getAuthorities().toString();
+        return currentRole.equalsIgnoreCase(ROLE_ADMIN);
+    }
 }
