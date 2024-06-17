84acb9e4c8ca6df3019582061ad8603c58ab2043
mirkocaforio <mirkocaforio2002@gmail.com>
2024-06-17 15:47:56 +0200

    Fix GlobalExceptionHandler

--- src/main/java/it/unisalento/pasproject/taskmanagerservice/exceptions/GlobalExceptionHandler.java
+++ src/main/java/it/unisalento/pasproject/taskmanagerservice/exceptions/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
